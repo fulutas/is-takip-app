1e890d81af33c05ce68eca6933041810beb3e5ef
furkanulutas <ulutasfurkan@gmail.com>
2021-05-09 00:19:24 +0300

    cookie ve signup işlemleri

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser') // cookie oluşturma
+
 const app = express();
 
 app.use(express.static('public'))
 app.use(express.json())
+app.use(cookieParser())
 
 app.set('view engine', 'ejs')
 
@@ -29,3 +32,25 @@ app.get('/works', (req,res) => {
 })
 
 app.use(authRoutes)
+
+
+// Cookie oluşturma (alternatif)
+app.get('/set-cookie', (req,res)=> {
+    res.setHeader('Set-Cookie', 'yeni=true')
+    res.send('Cookie oluştu')
+})
+
+/* Paket ile cookie oluşturma */
+app.get('/set-cookies', (req,res) => {
+    res.cookie('yeni',false)
+    res.cookie('parola','12345', {maxAge : 1000*60*60*24, httpOnly : true}) // 1 gün süre verilir. HttpOnly ile daha güvenli olması sağlanır.
+    res.send('Cookie oluştu')
+})
+
+// Kayıt olan Cookie lere ulaşmak
+app.get('/get-cookies', (req,res)=>{
+
+    const cookies = req.cookies
+    console.log(cookies.yeni)
+    res.json(cookies)
+})
