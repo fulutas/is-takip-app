46defc78c0b6594bf1bd5f6b127b84f0f7e4cacd
furkanulutas <ulutasfurkan@gmail.com>
2021-05-09 18:55:59 +0300

    iş ekleme view routes controller eklendi 2

--- controllers/workController.js
+++ controllers/workController.js
@@ -1,3 +1,27 @@
-module.exports.work_add_get = (req,res) => {
+const Work = require('../models/Work')
+
+module.exports.work_add_get = (req, res) => {
     res.render('work-add')
+}
+
+module.exports.work_add_post = async (req, res) => {
+    // HTTP request verileri al
+    const { baslik, aciklama, baslama, bitis } = req.body
+
+    try {
+        // Verileri db ekler.
+        const work = await Work.create({
+            baslik,
+            aciklama,
+            baslamaZamani: new Date(baslama),
+            bitisZamani: new Date(bitis),
+            kullaniciId: res.locals.user._id
+        })
+
+        res.status(200).json({ work: work._id, status: "Başarılı eklendi" })
+
+    } catch (error) {
+        res.status(400).json({ error: error, status: "Hata! Eklenemedi." })
+
+    }
 }
--- routes/workRoutes.js
+++ routes/workRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const workController = require('../controllers/workController')
-const { authKontrol } = require('../middleware/authMiddleware')
+const { authKontrol, kullaniciKontrol } = require('../middleware/authMiddleware')
 
 const router = Router()
 
 router.get('/work-add', authKontrol, workController.work_add_get)
+router.post('/work-add', kullaniciKontrol, workController.work_add_post)
 
 module.exports = router
